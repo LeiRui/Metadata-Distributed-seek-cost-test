897422c07a3506a8678ac6bc6ea9286b3c636131
RuiLei <ruileisurprise@gmail.com>
2018-01-30 11:52:36 +0800

    linux

--- src/Test.java
+++ src/Test.java
@@ -3,15 +3,25 @@ import java.io.PrintStream;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        File result = new File("files/result4.csv");
+        File result = new File("/home/liurui/testTemp/metadataTest/files/result5.csv");
+        File result2 = new File("/home/liurui/testTemp/metadataTest/files/result6.csv");
         if(result.exists()){
             result.delete();
         }
+        if(result2.exists()){
+            result2.delete();
+        }
         PrintStream out = new PrintStream(result);
+        PrintStream out2 = new PrintStream(result2);
         out.println("stripeNum,stripeSize,blockSize,intervalSize,,completeTime" +
                 ",distributedTime(total),distributedTimefirstSeek" +
                 ",comSeekTime" +
                 ",disSeekTime,seek length(include),seek length(exclude)");
+        out2.println("stripeNum,stripeSize,blockSize,intervalSize,,completeTime" +
+                ",distributedTime(total),distributedTimefirstSeek" +
+                ",comSeekTime" +
+                ",disSeekTime,seek length(include),seek length(exclude)");
+
 
         //第一组实验：改变stripeSize
 
@@ -20,9 +30,9 @@ public class Test {
         long[] stripeSizeTest = new long[]{100000,50000,10000,5000,1000,   1000, 1000, 1000, 1000, 1000, 1000};
         long[] intervalSizeTest = new long[]{10000,10000,10000,10000,10000,  100, 500, 1000, 5000, 20000, 50000};
         */
-        long[] stripeNumTest = new long[]{500,1000,5000,10000,50000,  50000,100000,150000,200000,   50000,50000,50000,50000,50000,50000,  50000,50000,50000,50000,50000,50000,50000,50000 };
-        long[] stripeSizeTest = new long[]{100000,50000,10000,5000,1000,  1000,1000,1000,1000,   1000,2000,5000,8000,10000,20000,   1000,1000,1000,1000,1000,1000,1000,1000};
-        long[] intervalSizeTest = new long[]{10000,10000,10000,10000,10000,   10000,10000,10000,10000,   10000,10000,10000,10000,10000,10000,   100,500,1000,5000,10000,15000,20000,50000};
+        long[] stripeNumTest = new long[]{500,1000,5000,10000,50000,  50000,100000,150000,200000,250000,     10000,20000,30000,40000,      50000,50000,50000,50000,50000,50000,50000,50000,  50000,50000,50000,50000,50000,50000,50000,50000 };
+        long[] stripeSizeTest = new long[]{100000,50000,10000,5000,1000,  1000,1000,1000,1000,1000,        5000,5000,5000,5000,     500,1000,1500,2000,5000,8000,10000,20000,   1000,1000,1000,1000,1000,1000,1000,1000};
+        long[] intervalSizeTest = new long[]{10000,10000,10000,10000,10000,   10000,10000,10000,10000,10000,   50000,50000,50000,50000,   10000,10000,10000,10000,10000,10000,10000,10000,   100,500,1000,5000,10000,15000,20000,50000};
         //long[] stripeNumTest = new long[]{50000, 100000, 150000, 200000,      50000, 50000,50000,50000,       50000,50000,50000,50000   };
         //long[] stripeSizeTest = new long[]{1000, 1000, 1000, 1000,          5000, 8000,10000,20000,           1000,1000,1000,1000};
         //long[] intervalSizeTest = new long[]{10000, 10000, 10000, 10000,     10000, 10000,10000,10000,        10000,15000,20000,50000};
@@ -49,30 +59,38 @@ public class Test {
                 FileTool fileTool = new FileTool(parameter);
 
                 //step 3 生成并写文件
-                String comPath = "files/complete.txt";
-                String disPath = "files/distributed.txt";
+                String comPath = "/home/liurui/testTemp/metadataTest/files/complete.txt";
+                String disPath = "/home/liurui/testTemp/metadataTest/files/distributed.txt";
                 fileTool.fileGenerator(comPath, disPath);
 
                 File f = new File(disPath);
-                seekLengthInclude += fileTool.stripesIndex.costInclude(f.length());// note 这一步得在此时执行 因为后面会把index清空
-                seekLengthExclude += fileTool.stripesIndex.costExclude();// note 这一步得在此时执行 因为后面会把index清空
+                long seekLengthIncludetmp = fileTool.stripesIndex.costInclude(f.length());// note 这一步得在此时执行 因为后面会把index清空
+                long seekLengthExcludetmp = fileTool.stripesIndex.costExclude();// note 这一步得在此时执行 因为后面会把index清空
+                seekLengthInclude += seekLengthIncludetmp;
+                seekLengthExclude += seekLengthExcludetmp;
 
                 //step 4 文件读取代价测试
-                com += fileTool.testComplete();
+                long comtmp = fileTool.testComplete();
+                com += comtmp;
                 long dis[] = fileTool.testDistributed();
                 dis0+=dis[0];
                 dis1+=dis[1];
-                comOnly+=fileTool.testCompleteOnlySeek();
-                disOnly+=fileTool.testDistributedOnlySeek();
-
+                long comOnlytmp = fileTool.testCompleteOnlySeek();
+                long disOnlytmp = fileTool.testDistributedOnlySeek();
+                comOnly+=comOnlytmp;
+                disOnly+=disOnlytmp;
+                out2.println(stripeNum+","+stripeSize + "," +stripeNum*stripeSize+","+ intervalSize + ",,"
+                        + (double)comtmp + "," + (double)dis[1]+ "," + (double)dis[0]
+                        + "," + (double)comOnlytmp+ "," + (double)disOnlytmp + "," + (double)seekLengthIncludetmp
+                        + "," + (double)seekLengthExcludetmp);
             }
             //step 5 返回结果
+            out2.println("");
             out.println(stripeNum+","+stripeSize + "," +stripeNum*stripeSize+","+ intervalSize + ",,"
                     + (double)com/loopNum + "," + (double)dis1/loopNum + "," + (double)dis0/loopNum
                     + "," + (double)comOnly/loopNum + "," + (double)disOnly/loopNum + "," + (double)seekLengthInclude/loopNum
                     + "," + (double)seekLengthExclude/loopNum);
         }
-
         /*
         //第二组实验：改变intervalSize
         //long[] intervalSizeTest = new long[]{10, 100, 500, 1000, 10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000};
@@ -116,6 +134,7 @@ public class Test {
         }
         */
         out.close();
+        out2.close();
 
                /*
             System.out.println(parameter);
